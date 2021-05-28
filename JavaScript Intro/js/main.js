45efd3c9d060303d9897e79b8766f1f4fb222159
DanHausen <danihausen@live.com>
2021-05-28 20:32:45 -0300

    IntroJS

---
+++ JavaScript Intro/js/main.js
@@ -0,0 +1,58 @@
+//Testing
+
+// var myName = "Daniel";
+// var age = 25;
+// var year = 1995;
+//alert("Oi, " + myName + ". Você possui " + age + " anos e nasceu em " + year);
+//alert(frase.replace("2021", "2022"));
+
+// var frase = "Hoje é maio de 2021";
+// console.log("Teste no console da web");
+// console.log(frase.replace("maio", "junho"));
+
+// var lista = ["Daniel", "Ana", "Mônica"]; //list
+// console.log(lista);
+// lista.push("Nero");
+// console.log(lista);
+// console.log(lista.length);
+// lista.pop();
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" | "));
+
+// var games = [{name: "Halo", ano: "2021", nomeCompleto: "Halo Infinite"}, {name: "Zelda", ano: "2022", nomeCompleto: "The Legend of Zelda Breath of the Wild 2"}, {name: "God of War", ano: "2023", nomeCompleto: "God of War Ragnarok"}]; //dictionary
+// console.log(games);
+// console.log(games[0].nomeCompleto);
+
+// var idade = prompt("Me diga sua idade");
+// if(idade >= 18)
+// alert("Maior de idade");
+// else
+// alert("Menor de idade");
+
+// var maximo = prompt("Digite o valor maximo");
+// var contador = 0;
+// while (contador <= maximo){
+//     console.log(contador);
+//     contador++;
+// }
+
+// fibonacci(prompt("Insira o limite"));
+// function fibonacci(num){
+//     var a = 1, b = 0, temp;  
+//     while (num >= 0){
+//       temp = a;
+//       a = a + b;
+//       b = temp;
+//       console.log(b);
+//       num--;
+//     }  
+//     return b;
+//   }
+
+var d = new Date();
+console.log(d);
+console.log(d.getMonth() + 1);
+console.log(d.getMinutes());
