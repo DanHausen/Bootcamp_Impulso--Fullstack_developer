928e4f0c5db0539b7cf4c15bf21a3fd6125d2077
DanHausen <danihausen@live.com>
2021-06-10 18:35:52 -0300

    Exercise_resolved

--- RubyOOP/Exercises/bank.rb
+++ RubyOOP/Exercises/bank.rb
@@ -1,14 +1,31 @@
-class Banks
-    nubank = 300
-    inter = 200
-    
-    def transfer(account, amount)   
-        
-    end
-    
-    def amount
-        #return amount
+class Tasks    
+    def transfer(origin, destination, amount)   
+        amount < 0 ? amount *= -1 : amount *= 1
+        origin.amount -= amount    
+        destination.amount += amount
     end
 end
 
-obj = Banks.new
+class Nubank
+    attr_accessor :amount
+    
+    def initialize(amount)
+        @amount = amount
+    end
+end
+
+class Inter
+    attr_accessor :amount
+    
+    def initialize(amount)
+        @amount = amount
+    end
+end
+
+nubank = Nubank.new(0)
+inter = Inter.new(0)
+
+tasks = Tasks.new
+tasks.transfer(inter, nubank, 100)
+p inter.amount
+p nubank.amount
