f8ff40a3bde3aec0039051dffe177b8984b5dd0f
DanHausen <danihausen@live.com>
2021-06-14 20:25:03 -0300

    Numbers

---
+++ JavaScript ES6 essencial/numbers.js
@@ -0,0 +1,11 @@
+const myNum = 12.1995
+
+const numberAsString = myNum.toString();
+console.log('Número transformado em String: ', numberAsString);
+
+const fixedTwoDecimals = myNum.toFixed(2);
+console.log('\nNúmero com duas casas decimais: ', fixedTwoDecimals);
+
+console.log('\nString parseada para float: ',parseFloat('552.8'));
+
+console.log('\nString parseada para integer: ',parseInt('55.27'));
