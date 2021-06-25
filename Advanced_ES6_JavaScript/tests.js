b9003fdde74d42df60b14cbb8cca66a4e060c435
DanHausen <danihausen@live.com>
2021-06-25 17:44:47 -0300

    main

---
+++ Advanced_ES6_JavaScript/tests.js
@@ -0,0 +1,24 @@
+//TESTES AUTOMATIZADOS:
+// Testes unitarios
+// Testes integrados;
+// Testes funcionais;
+
+//TESTES MANUAIS E AUTOMATIZADOS:
+// Testes de usabilidade
+// Testes de aceitação do usuário
+// Protótipos
+// Testes funcionais
+// Exemplos
+// Alpha e Beta
+// mais...
+
+//FERRAMENTAS DE TESTES:
+// Teste de carga e performance
+// Testes de segurança
+// mais...
+
+//TDD (TEST DRIVEN DEVELOPMENT)
+// É um dos pilares do Extreme programming, consiste em testar e refatorar em pequenos ciclos, onde você escreve o teste antes do código, faz o mesmo passar e refatora o código
+
+//BDD (BEHAVIOR DRIVEN DEVELOPMENT)
+// Técnica de desenvolvimento ágil que visa integrar regras de negócio com linguagens de programação
