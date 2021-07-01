c16bf40e97398700dad0ad4b9545b1ddbead64cd
danhausen <danihausen@live.com>
2021-07-01 12:05:47 -0300

    main

---
+++ Introduction_to_ReactJS/Historia.js
@@ -0,0 +1,12 @@
+// React é uma biblioteca JavaScript para criar interfaces de usuário, não é um framework
+// Criado em 2011 por Jordan Walke
+//Baseado em XHP, um framework para criação de HTML no PHP
+// A principal função é a criação da interface para o usuário
+// React é uma linguagem declarativa, ou seja, sua unica preocupação é se está sendo exibida ou não na tela.
+// React trabalha com componentes, podendo facilmente reutilizar esses componentes. Componentes trabalham como função e classe.
+// JS não possui classe, e sim prototipo.
+
+// Estado e ciclo de vida
+// É ideal aprender JS para essas ferramentas
+// Toda vez que a pagina é atualizada, o React refaz o render.
+// 
